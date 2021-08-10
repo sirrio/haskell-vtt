5e36dcfad389a18558dda6d321c0ba17d49d856e
david <home@sirrio.de>
2021-08-10 02:22:19 +0200

    Pushed to first prototype

--- Client.hs
+++ Client.hs
@@ -1,23 +1,193 @@
 {-# LANGUAGE OverloadedStrings #-}
-module Main (main) where
+module Main where
+import Network.Socket
+    ( socketToHandle,
+      getAddrInfo,
+      withSocketsDo,
+      connect,
+      socket,
+      defaultProtocol,
+      AddrInfo(addrFamily, addrAddress),
+      SocketType(Stream),
+      Socket, sendBuf )
+import System.IO
+    ( hSetBuffering,
+      hGetLine,
+      BufferMode(BlockBuffering, NoBuffering, LineBuffering),
+      IOMode(ReadWriteMode),
+      Handle, hFlush, char8)
+import qualified Data.ByteString.Char8 as Char8
+import GHC.IO.Handle (hFlushAll)
+import SDL
+import Linear (V4(..))
+import Control.Monad(void, unless, when, forM_)
+import Control.Monad.IO.Class(MonadIO (liftIO))
+import Data.Text(Text, empty)
+import Foreign.C.Types(CInt)
+import SDL.Input.Mouse (getRelativeMouseLocation)
+import Data.List
+import Data.Maybe
+import Control.Concurrent
+    ( dupChan, newChan, readChan, writeChan, forkIO, killThread, Chan, getChanContents )
+import Data.ByteString.Lazy (toStrict, fromStrict)
+import Control.Lens
+import Data.Map
+import Control.Monad.Fix (fix)
+import Data.List.Split
+import Control.Concurrent.STM
+import Text.Read (readMaybe)
 
-import Control.Monad (unless)
-import qualified Data.ByteString.Char8 as C
-import Network.Socket.ByteString (recv, sendAll)
-import Network.Run.TCP (runTCPClient)
+data World = World {
+  playerPos :: Point V2 Integer,
+  playerName :: String
+}
+
+type RemotePlayer = TVar (Map Integer (Integer, Integer))
+
+
+newRmtPlayerMap :: Map Integer (Integer, Integer) -> IO RemotePlayer
+newRmtPlayerMap = newTVarIO
+
+addRmtPlayer :: Integer -> (Integer, Integer) -> TVar (Map Integer (Integer, Integer)) -> STM ()
+addRmtPlayer id pos m = do
+    map <- readTVar m
+    let newmap = Data.Map.insert id pos map
+    writeTVar m newmap
+
+-- newRP :: [(String, Integer, Integer)] 
+
+-- data RemotePlayer = RemotePlayer { rpList :: Map String (Point V2 Integer) }
+
+initialWorld :: World
+initialWorld = World {
+  playerPos = P (V2 0 0),
+  playerName = "default"
+}
+
+-- initialRemotePlayer :: RemotePlayer 
+-- initialRemotePlayer = RemotePlayer {
+--   rpList = Data.Map.empty
+-- } 
+
+updateWorld :: World -> String -> Point V2 Integer -> World
+updateWorld w n p = w { playerPos = p,  playerName = n }
+
+type Msg = (Int, String)
 
 main :: IO ()
-main = runTCPClient "127.0.0.1" "3000" init
-    where
-        init s = do
-            putStrLn "Type your name:"
-            name <- C.getLine
-            talk s name
-        talk s name = do
-            putStrLn "Type messege: "
-            msg <- C.getLine 
-            sendAll s (C.append (C.append name ": ") msg)
-            msg <- recv s 1024
-            putStr ">"
-            C.putStrLn msg
-            talk s name
+main = do
+  initializeAll
+  rmtPlayer <- newRmtPlayerMap Data.Map.empty
+  addrInfos <- getAddrInfo Nothing (Just "127.0.0.1") (Just "3000")
+  socket <- socket (addrFamily (head addrInfos)) Stream defaultProtocol
+  connect socket (addrAddress (head addrInfos))
+  handle <- socketToHandle socket ReadWriteMode
+  hSetBuffering handle NoBuffering
+  putStrLn "Connection estabished: choose a name!"
+  playername <- Char8.getLine
+  Char8.hPut handle (Char8.append playername "s\n")
+  let updatedWorld = updateWorld initialWorld (Char8.unpack playername) (playerPos initialWorld)
+  -- let remotePlayer = Data.Map.empty
+  -- chan <- newChan
+  window <- createWindow "VTT Haskell" defaultWindow
+  renderer <- createRenderer window (-1) defaultRenderer
+  forkIO $ handleConnection handle rmtPlayer
+  appLoop updatedWorld handle rmtPlayer renderer
+  destroyWindow window
+
+appLoop :: World -> Handle -> TVar (Map Integer (Integer, Integer)) -> Renderer -> IO ()
+appLoop world handle rmtPlayer renderer = do
+  events <- pollEvents
+  let eventIsQPress event =
+        case eventPayload event of
+          KeyboardEvent keyboardEvent ->
+            keyboardEventKeyMotion keyboardEvent == Pressed &&
+            keysymKeycode (keyboardEventKeysym keyboardEvent) == KeycodeQ
+          _ -> False
+      qPressed = any eventIsQPress events
+  let mousePressedEvent event =
+        case eventPayload event of
+          MouseButtonEvent mouseButtonEvent ->
+            mouseButtonEventMotion mouseButtonEvent == Pressed
+          _ -> False
+      mousePressed = any mousePressedEvent events
+  let mouseMotionEventMap event =
+        case eventPayload event of
+          MouseMotionEvent mme -> Just mme
+          _ -> Nothing
+      mouseMotions = Data.Maybe.mapMaybe mouseMotionEventMap events
+
+  let w = updateWorld world (playerName world) (getMousePos mouseMotions (playerPos world))
+  -- Char8.hPut handle $ Char8.append (Char8.pack $ playerName world) $ Char8.append "." $ Char8.append (Char8.pack $ show (view _x (playerPos world))) $ Char8.append "." $ Char8.append (Char8.pack $ show (view _y (playerPos world))) "\n"
+  -- Char8.hPut handle (Char8.append (serialize (playerName world) (playerPos world)) "s\n")
+  Char8.hPut handle $ Char8.append (Char8.pack $ show (view _x (playerPos world))) $ Char8.append "." $ Char8.append (Char8.pack $ show (view _y (playerPos world))) "\n"
+
+  -- if mouseMoved then putStrLn "TRUE" else putStrLn "False"
+  -- if fmap (payloadToIntent . SDL.eventPayload) == Hover TopLeft then putStrLn "yes" else putStrLn "not"    
+    -- forM_ mouseMotions $ \s -> do
+  --     let lastPos = fmap fromIntegral (mouseMotionEventPos  s)
+  --     liftIO w <- updateWorld world (P (V2 15 15))
+  --     print lastPos
+
+  -- reader <- forkIO $ fix $ \loop -> do
+  --   -- (_, line):list <- getChanContents commLine
+  --   ( _, line) <- readChan chan
+  --   putStrLn line
+  --   loop
+
+    -- let name:x:y:xs = splitOn "x" line
+    -- putStrLn ("X: " ++ x)
+    -- putStrLn ("Y: " ++ y)
+    -- fillRect renderer (Just (Rectangle (P (V2 (read x) (read y))) (V2 32 32)))
+
+      -- fillRect renderer (Just (Rectangle (P (V2 (read x) (read y))) (V2 32 32)))
+  -- rendererDrawColor renderer $= V4 255 0 0 255
+  -- fillRect renderer (Just (Rectangle (P (V2 50 50)) (V2 32 32)))
+
+  clear renderer
+  rendererDrawColor renderer $= V4 133 133 133 255
+  fillRect renderer (Just (Rectangle (P (V2 0 0)) (V2 1024 800)))
+
+  rendererDrawColor renderer $= V4 255 0 0 255
+  rmtPlayerMap <- readTVarIO rmtPlayer
+  mapM_ (\(_,(x, y)) ->   fillRect renderer (Just (Rectangle (P (V2 (fromIntegral x) (fromIntegral y))) (V2 32 32)))) (toList rmtPlayerMap)
+
+  rendererDrawColor renderer $= V4 255 0 0 255
+  fillRect renderer (Just (Rectangle (P (V2 50 50)) (V2 32 32)))
+  rendererDrawColor renderer $= V4 0 0 0 255
+  fillRect renderer (Just (Rectangle (fmap fromIntegral (playerPos world)-P (V2 16 16)) (V2 32 32)))
+  present renderer
+  unless qPressed (appLoop w handle rmtPlayer renderer)
+
+
+-- serialize :: p -> Point V2 Integer -> Char8.ByteString
+-- serialize name location = toStrict $ encode(42 :: Integer)
+-- -- serialize name location = "helloworld"
+
+
+getMousePos :: Num b => [MouseMotionEventData] -> Point V2 b -> Point V2 b
+getMousePos (x:xs) _ = fmap fromIntegral (mouseMotionEventPos  x)
+getMousePos _ p = p
+
+
+handleConnection :: Handle -> TVar (Map Integer (Integer, Integer))-> IO b
+handleConnection handle rmtPlayer = do
+  l <- hGetLine handle
+  -- putStrLn l
+  let id:x:y:xs = splitOn "." l
+  let intid = readMaybe id
+  let intx = readMaybe x
+  let inty = readMaybe y
+  case (intid, intx, inty) of
+    (Just id, Just x, Just y) -> atomically $ addRmtPlayer id (x, y) rmtPlayer
+    _ -> putStrLn "Parsing error"
+
+  -- let map = Data.Map.insert "1" (P (V2 0 0)) Data.Map.empty
+  -- let broadcast msg = writeChan chan (0, msg)
+  -- broadcast l
+  handleConnection handle rmtPlayer
+
+
+
+
+
--- Server.hs
+++ Server.hs
@@ -1,21 +1,70 @@
 {-# LANGUAGE OverloadedStrings #-}
-module Main (main) where
-
-import Control.Monad (unless)
-import qualified Data.ByteString as S
-import qualified Data.ByteString.Char8 as C
-import Network.Run.TCP (runTCPServer)
-import Network.Socket.ByteString (recv, sendAll)
-
-data TurnstileSate = Locked | Unlocked
-  deriving (Eq, Show)
+module Main where
+import Network.Socket
+    ( Socket,
+      SockAddr(SockAddrInet),
+      Family(AF_INET),
+      SocketType(Stream),
+      socketToHandle,
+      setSocketOption,
+      accept,
+      bind,
+      listen,
+      socket,
+      SocketOption(ReuseAddr) )
+import System.IO
+    ( hClose,
+      hSetBuffering,
+      hGetLine,
+      hPutStrLn,
+      BufferMode(NoBuffering),
+      IOMode(ReadWriteMode) )
+import Control.Exception ( SomeException(SomeException), handle )
+import Control.Concurrent
+    ( dupChan, newChan, readChan, writeChan, forkIO, killThread, Chan, threadDelay )
+import Control.Monad (when)
+import Control.Monad.Fix (fix)
+import qualified System.IO as C
 
 main :: IO ()
-main = runTCPServer Nothing "3000" talk
-  where
-    talk s = do
-        msg <- recv s 1024
-        unless (S.null msg) $ do
-          sendAll s msg
-          C.putStrLn msg
-          talk s
+main = do
+  sock <- socket AF_INET Stream 0
+  setSocketOption sock ReuseAddr 1
+  bind sock (SockAddrInet 3000 0)
+  listen sock 2
+  chan <- newChan
+  mainLoop sock chan 0
+
+type Msg = (Int, String)
+
+mainLoop :: Socket -> Chan Msg -> Int -> IO ()
+mainLoop sock chan msgNum = do
+  conn <- accept sock -- RUNS WHEN NEW CLIENT CONNECTS
+  forkIO $ runConn conn chan msgNum
+  mainLoop sock chan $! msgNum + 1
+
+runConn :: (Socket, SockAddr) -> Chan Msg -> Int -> IO ()
+runConn (sock, _) chan msgNum = do
+    let broadcast msg = writeChan chan (msgNum, msg)
+    hdl <- socketToHandle sock ReadWriteMode
+    hSetBuffering hdl NoBuffering
+    name <- fmap init (hGetLine hdl)
+    putStrLn (name ++ " connected to the server! Player assigned ID " ++ show msgNum)
+    -- hPutStrLn hdl name
+
+    commLine <- dupChan chan
+
+    reader <- forkIO $ fix $ \loop -> do
+        (nextNum, line) <- readChan commLine
+        when (msgNum /= nextNum) $ hPutStrLn hdl (show msgNum ++ "." ++ line)
+        loop
+
+    handle (\(SomeException _) -> return ()) $ fix $ \loop -> do
+        line <- fmap init (hGetLine hdl)
+        -- threadDelay 1000 
+        broadcast  line
+        -- putStrLn line
+        loop
+
+    killThread reader
+    hClose hdl
